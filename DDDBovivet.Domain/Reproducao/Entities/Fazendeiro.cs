cb8a3ef2c51f2b1bca8a238798b331b5dde555e3
OtonielMM <estudantedaniel@hotmail.com>
2019-04-22 15:09:41 -0300

    Ajustes no VO Ende e Ent Fazendeiro

--- DDDBovivet.Domain/Reproducao/Entities/Fazendeiro.cs
+++ DDDBovivet.Domain/Reproducao/Entities/Fazendeiro.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DDDBovivet.Domain.Reproducao.ValueObjects;
+using DDDBovivet.Shared.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,12 @@ using System.Threading.Tasks;
 
 namespace DDDBovivet.Domain.Reproducao.Entities
 {
-    public class Fazendeiro
+    public class Fazendeiro : Entity
+
     {
         public Nome Nome { get; private set; }
         public Email Email { get; set; }
+        public Endereco Endereco { get; private set; }
         public string Cnpj { get; set; }
     }
 }
--- DDDBovivet.Domain/Reproducao/ValueObjects/Endereco.cs
+++ DDDBovivet.Domain/Reproducao/ValueObjects/Endereco.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DDDBovivet.Shared.ValueObject;
+using Flunt.Validations;
 
 namespace DDDBovivet.Domain.Reproducao.ValueObjects
 {
